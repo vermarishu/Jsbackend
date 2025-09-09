c449ea1584304317d65c2a0173acf0a5c77ba3d3
Rishu Verma <vermarishu251@gmail.com>
2025-09-09 23:53:06 +0530

    import and add router in app.js

--- src/app.js
+++ src/app.js
@@ -16,8 +16,14 @@ app.use(cookieParser())
 
 // import Router
 import userRouter from './routes/user.routes.js'
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("api/v1/videos", videoRouter)
+
+
 
 export { app }
